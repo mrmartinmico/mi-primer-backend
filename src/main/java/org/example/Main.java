48499fdc92816a7ec27531f85263b5fb4015f3fc
Martin <martinmico3@gmail.com>
2026-07-01 19:05:16 +0100

    Fase 4 completada: Persistencia real con PostgreSQL en Docker funcionando

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,29 +1,12 @@
 package org.example;
 
-import org.example.model.Videojuego;
-import org.example.repository.VideojuegoRepository;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class Main {
 
     public static void main(String[] args) {
         SpringApplication.run(Main.class, args);
-    }
-
-    @Bean
-    public CommandLineRunner ejecutarAlArranque(VideojuegoRepository repo) {
-        return args -> {
-            // Guardamos tres videojuegos de prueba en nuestra base de datos H2
-            repo.save(new Videojuego("The Legend of Zelda: Tears of the Kingdom", "Nintendo Switch", 2023));
-            repo.save(new Videojuego("Elden Ring", "PlayStation 5", 2022));
-            repo.save(new Videojuego("Cyberpunk 2077", "PC", 2020));
-            repo.save(new Videojuego("Minecraft", "PC", 2011));
-            repo.save(new Videojuego("Gran Turismo 7", "PlayStation 5", 2022));
-            System.out.println(">> ¡Videojuegos de prueba cargados con éxito en la base de datos! <<");
-        };
     }
 }
